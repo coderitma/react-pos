d449e16aa0ef3904107643e8faf71f8c97c45191
Yanzen Project <yanwarsolah@gmail.com>
2023-03-29 19:14:34 +0700

    step 01

--- src/pages/POSPage.js
+++ src/pages/POSPage.js
@@ -1,7 +1,32 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+
 const POSPage = () => {
+  const inlineCard = (productName) => {
+    return (
+      <Col md={3}>
+        <Card>
+          <Card.Body>
+            <Card.Title>{productName}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  };
+
   return (
     <>
-      <h1>Hello World POS Page</h1>
+      <Container>
+        <Row>
+          <Col md={8}>
+            <Row>
+              {inlineCard("Produk 1")}
+              {inlineCard("Produk 2")}
+              {inlineCard("Produk 3")}
+            </Row>
+          </Col>
+          <Col md={4}>Panel Transaksi</Col>
+        </Row>
+      </Container>
     </>
   );
 };
