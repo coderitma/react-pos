f4f81e0eef05548fad11ced26f4240f92f6e6ac9
Yanzen Project <yanwarsolah@gmail.com>
2023-03-30 19:05:58 +0700

    step 13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,14 @@
+const helperDuplicatedInArrayObject = (item, by, array) => {
+  let isDuplicate = false;
+  for (let obj of array) {
+    if (item[by] === obj[by]) {
+      isDuplicate = true;
+    }
+  }
+
+  return isDuplicate;
+};
+
 const helperReadableCurrency = (num) => {
   let n = parseInt(num).toLocaleString("id-ID", {
     style: "currency",
@@ -6,4 +17,4 @@ const helperReadableCurrency = (num) => {
   return n;
 };
 
-export { helperReadableCurrency };
+export { helperReadableCurrency, helperDuplicatedInArrayObject };
