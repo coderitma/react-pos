fed980af9c23f1c08cfbcdaa6f52edc5777a55c3
Yanzen Project <yanwarsolah@gmail.com>
2023-03-31 20:20:37 +0700

    reset all

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import POSPage from "./pages/POSPage";
-import POSPrintPage from "./pages/POSPrintPage";
-import ContainerOutletWidget from "./widgets/commons/ContainerOutletWidget";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ContainerOutletWidget />}>
+        {/* <Route path="/" element={<ContainerOutletWidget />}>
           <Route index element={<LoginPage />} />
           <Route path="/pos" element={<POSPage />} />
           <Route path="/pos/print" element={<POSPrintPage />} />
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   );
--- src/Yanwar.js
+++
@@ -1,5 +0,0 @@
-function Yanwar() {
-  return <h1>Ini Yanwar</h1>;
-}
-
-export default Yanwar;
