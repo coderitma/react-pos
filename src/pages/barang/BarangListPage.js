093e7b3749604c8a956978f8ba3f17d70d70a2ba
Yanzen Project <yanwarsolah@gmail.com>
2023-04-03 21:06:38 +0700

    step 09 / h16

--- src/pages/barang/BarangListPage.js
+++ src/pages/barang/BarangListPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import HTTPService from "../../services/HTTPService";
 import AuthService from "../../services/AuthService";
+import { Card, Table } from "react-bootstrap";
 
 const BarangListPage = () => {
   const [daftarBarang, setDaftarBarang] = useState([]);
@@ -17,7 +18,32 @@ const BarangListPage = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  return <pre>{JSON.stringify(daftarBarang)}</pre>;
+  return (
+    <Card>
+      <Table>
+        <thead>
+          <tr>
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Harga Jual</th>
+            <th>Harga Beli</th>
+            <th>Jumlah Barang</th>
+          </tr>
+        </thead>
+        <tbody>
+          {daftarBarang.map((barang, index) => (
+            <tr key={index}>
+              <td>{barang.kodeBarang}</td>
+              <td>{barang.namaBarang}</td>
+              <td>{barang.hargaJual}</td>
+              <td>{barang.hargaBeli}</td>
+              <td>{barang.jumlahBarang}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Card>
+  );
 };
 
 export default BarangListPage;
