39829b54b683ba29a6a26453a206f2ebc51a2a5d
Yanzen Project <yanwarsolah@gmail.com>
2023-04-04 19:10:57 +0700

    step 01/17

--- src/services/HTTPService.js
+++ src/services/HTTPService.js
@@ -4,19 +4,35 @@ const HTTPService = axios.create({
   timeout: 1000,
 });
 
-HTTPService.interceptors.request.use((req) => {
-  document.body.classList.add("loading-indicator");
-  return req;
-});
-
-HTTPService.interceptors.response.use((res) => {
-  document.body.classList.remove("loading-indicator");
-
-  if (res.status === 401) {
-    window.location.href = "/";
+HTTPService.interceptors.request.use(
+  (req) => {
+    document.body.classList.add("loading-indicator");
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
+);
 
-  return res;
-});
+HTTPService.interceptors.response.use(
+  (res) => {
+    document.body.classList.remove("loading-indicator");
+    return res;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = "/";
+    }
+
+    if (error.response.status === 400 || error.response.status === 404) {
+      if (error.response.data.message) {
+        alert(error.response.data.message);
+      } else {
+        alert(error);
+      }
+    }
+    document.body.classList.remove("loading-indicator");
+  }
+);
 
 export default HTTPService;
