0a95317323eb224fc5dfc44da36c422116ea25b5
Yanzen Project <yanwarsolah@gmail.com>
2023-04-05 19:20:01 +0700

    step 03/18

--- src/pages/barang/BarangAddPage.js
+++ src/pages/barang/BarangAddPage.js
@@ -2,6 +2,13 @@ import { useState } from "react";
 
 const BarangAddPage = () => {
   const [barang, setBarang] = useState({});
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setBarang((values) => ({ ...values, [name]: value }));
+  };
 };
 
 export default BarangAddPage;
